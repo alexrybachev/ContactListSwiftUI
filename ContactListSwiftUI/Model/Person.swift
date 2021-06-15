ca896b2f44d11373d7fb5f2a0108adcf5231d2a3
Alex Rybachev <rybachev.as@gmail.com>
2021-06-15 21:23:10 +0400

    1) set Model 2) set DataManager

---
+++ ContactListSwiftUI/Model/Person.swift
@@ -0,0 +1,50 @@
+//
+//  Person.swift
+//  ContactListSwiftUI
+//
+//  Created by Aleksandr Rybachev on 15.06.2021.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        
+        let iterationCount = min(names.count, surnames.count, phones.count, emails.count)
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                phone: phones[index],
+                email: emails[index]
+            )
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone"
+    case email = "email"
+}
